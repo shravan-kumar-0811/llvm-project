80f522f483ff57f9fada0834d44fc7408c91d368
Samuel Tebbs <samuel.tebbs@arm.com>
2024-07-17 16:40:04 +0100

    Fix build error

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -7989,7 +7989,7 @@ void SelectionDAGBuilder::visitIntrinsicCall(const CallInst &I,
   }
   case Intrinsic::experimental_vector_partial_reduce_add: {
 
-    if (!TLI.shouldExpandPartialReductionIntrinsic(&I))) {
+    if (!TLI.shouldExpandPartialReductionIntrinsic(&I)) {
         visitTargetIntrinsic(I, Intrinsic);
         return;
       }
