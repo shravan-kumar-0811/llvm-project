160c464924326ab82caa59ce77491e67f1387e5c
Krystian Stasiowski <sdkrystian@gmail.com>
2024-07-18 09:51:28 -0400

    [FOLD] emit better missing 'template' keyword diagnostics for dependent class member access

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -1967,7 +1967,7 @@ private:
   //===--------------------------------------------------------------------===//
   // C++ Expressions
   ExprResult tryParseCXXIdExpression(CXXScopeSpec &SS, bool isAddressOfOperand,
-                                     Token &Replacement);
+                                     Token *Replacement = nullptr);
 
   ExprResult tryParseCXXPackIndexingExpression(ExprResult PackIdExpression);
   ExprResult ParseCXXPackIndexingExpression(ExprResult PackIdExpression);
