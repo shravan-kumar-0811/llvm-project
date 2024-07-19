1c530da0206fab1c2028cfd81f088b59a612ffe4
Wang Pengcheng <wangpengcheng.pp@bytedance.com>
2024-07-19 15:54:24 +0800

    Address comments

--- openmp/runtime/src/kmp_affinity.h
+++ openmp/runtime/src/kmp_affinity.h
@@ -276,18 +276,7 @@ public:
 #elif __NR_sched_getaffinity != 123
 #error Wrong code for getaffinity system call.
 #endif /* __NR_sched_getaffinity */
-#elif KMP_ARCH_RISCV32
-#ifndef __NR_sched_setaffinity
-#define __NR_sched_setaffinity 122
-#elif __NR_sched_setaffinity != 122
-#error Wrong code for setaffinity system call.
-#endif /* __NR_sched_setaffinity */
-#ifndef __NR_sched_getaffinity
-#define __NR_sched_getaffinity 123
-#elif __NR_sched_getaffinity != 123
-#error Wrong code for getaffinity system call.
-#endif /* __NR_sched_getaffinity */
-#elif KMP_ARCH_RISCV64
+#elif KMP_ARCH_RISCV64 || KMP_ARCH_RISCV32
 #ifndef __NR_sched_setaffinity
 #define __NR_sched_setaffinity 122
 #elif __NR_sched_setaffinity != 122
