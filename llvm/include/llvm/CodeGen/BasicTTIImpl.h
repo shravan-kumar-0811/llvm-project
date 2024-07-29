2169c487ff0dfd2375e221b1eb06d709c694cc41
Sumanth Gundapaneni <sumanth.gundapaneni@amd.com>
2024-07-29 13:02:10 -0500

    Scalarize the vector inputs to llvm.lround intrinsic by default.
    Verifier is updated in a different patch to let the vector types for
    llvm.lround and llvm.llround intrinsics.

--- llvm/include/llvm/CodeGen/BasicTTIImpl.h
+++ llvm/include/llvm/CodeGen/BasicTTIImpl.h
@@ -2063,6 +2063,12 @@ public:
     case Intrinsic::roundeven:
       ISD = ISD::FROUNDEVEN;
       break;
+    case Intrinsic::lround:
+      ISD = ISD::LROUND;
+      break;
+    case Intrinsic::llround:
+      ISD = ISD::LLROUND;
+      break;
     case Intrinsic::pow:
       ISD = ISD::FPOW;
       break;
